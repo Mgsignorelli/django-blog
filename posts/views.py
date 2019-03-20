fd6c0d1211e0a13fafcd92ab973a36fc3f32590c
Micaela Signorelli <m.signorelli@gmail.com>
2019-03-20 19:09:16 +0000

    fixes errors in posts directory

--- posts/views.py
+++ posts/views.py
@@ -6,25 +6,25 @@ from .forms import BlogPostForm
 def get_posts(request):
     """
     Create a view that will return a list
-    of Posts that were published prior to 'now'
-    and render them to the 'blogposts.html' template
+    of Posts that were published prior to "now"
+    and render them to the "blogposts.html" template
     """
     
-    posts = Post.objects.filter(published_date=timezone.now()).order_by('-published_date')
-    return render(request, "blogposts.html", {'posts': posts})
+    posts = Post.objects.filter(published_date=timezone.now()).order_by("-published_date")
+    return render(request, "blogposts.html", {"posts": posts})
 
 def post_detail(request, pk):
     """
     Create a view that returns a single
     Post object based on the post ID (pk) and
-    render it to the 'postdetail.html' template.
+    render it to the "postdetail.html" template.
     Or return a 404 error if the post is
     not found
     """
     post = get_object_or_404(Post, pk=pk)
     post.views += 1
     post.save()
-    return render(request, "postdetail.html", {'post': post})
+    return render(request, "postdetail.html", {"post": post})
 
 def create_or_edit_post(request, pk=None):
     """
@@ -40,5 +40,5 @@ def create_or_edit_post(request, pk=None):
             return redirect(post_detail, post.pk)
     else:
         form = BlogPostForm(instance=post)
-    return render(request, 'blogpostform.html', {'form': form})
+    return render(request, "blogpostform.html", {"form": form})
     
